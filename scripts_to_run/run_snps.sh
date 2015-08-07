977c60a7ff5d7c9be3e6f031b8903acb1fdf3ae9
ecgberg <eglassbe@stanford.edu>
2015-08-07 10:06:33 -0700

    Added the scripts to run STR or SNP simulations via qsub

---
+++ scripts_to_run/run_snps.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+#$ -N run_SNPs_1
+#
+#$ -l h_vmem=4G
+#
+#$ -l h_rt=5:59:59
+#
+#$ -m eab
+#
+#$ -M eglassbe@stanford.edu
+#
+#$ -w e
+#
+#$ -e /home/eglassbe/strs/jobs/snp_1.err
+#
+#$ -cwd
+#
+#$ -o /home/eglassbe/strs/jobs/snp_1.out
+#
+
+module load python/2.7
+
+mu=.0005
+
+beta=1
+
+path='/home/eglassbe/strs/str_res/'
+
+python str_sims.py -m $mu -b $beta -p $path
---
+++ scripts_to_run/run_strs.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+#$ -N run_STRs_1
+#
+#$ -l h_vmem=4G
+#
+#$ -l h_rt=5:59:59
+#
+#$ -m eab
+#
+#$ -M eglassbe@stanford.edu
+#
+#$ -w e
+#
+#$ -e /home/eglassbe/strs/jobs/str_1.err
+#
+#$ -cwd
+#
+#$ -o /home/eglassbe/strs/jobs/str_1.out
+#
+
+module load python/2.7
+
+mu=.0005
+p=0.84
+sigsq_g=20
+
+beta=0.5
+
+path='/home/eglassbe/strs/str_res/'
+
+python str_sims.py -m $mu $p $sigsq_g -b $beta -p $path
