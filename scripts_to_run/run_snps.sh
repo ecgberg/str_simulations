8c0d17fc8c923804c0e50afcc34b6b6c90c74050
ecgberg <eglassbe@stanford.edu>
2015-08-13 14:49:30 -0700

    first results on when SNP runs crash as a fxn of the betas

--- scripts_to_run/run_snps.sh
+++ scripts_to_run/run_snps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#$ -N run_SNPs_1
+#$ -N run_SNPs_9
 #
 #$ -l h_vmem=4G
 #
@@ -12,19 +12,30 @@
 #
 #$ -w e
 #
-#$ -e /home/eglassbe/str_simulations/jobs/snp_1.err
+#$ -e /home/eglassbe/str_simulations/jobs/snp_9.err
 #
 #$ -cwd
 #
-#$ -o /home/eglassbe/str_simulations/jobs/snp_1.out
+#$ -o /home/eglassbe/str_simulations/jobs/snp_9.out
 #
 
 module load python/2.7
 
 mu=.0005
 
-beta=1
+beta=( 1 0.9 0.8 0.7 0.6 0.5 0.4 0.2 0.3 0.1 )
 
-path='/home/eglassbe/str_simulations/str_res/'
+runnum=9
 
-python str_sims.py -m $mu -b $beta -p $path
+path='/home/eglassbe/str_simulations/str_res/run_'$runnum'/'
+
+python str_sims.py -m $mu -b ${beta[0]} -p $path -w 10
+python str_sims.py -m $mu -b ${beta[1]} -p $path -w 10
+python str_sims.py -m $mu -b ${beta[2]} -p $path -w 10
+python str_sims.py -m $mu -b ${beta[3]} -p $path -w 10
+python str_sims.py -m $mu -b ${beta[4]} -p $path -w 10
+python str_sims.py -m $mu -b ${beta[5]} -p $path -w 10
+python str_sims.py -m $mu -b ${beta[6]} -p $path -w 10
+python str_sims.py -m $mu -b ${beta[7]} -p $path -w 10
+python str_sims.py -m $mu -b ${beta[8]} -p $path -w 10
+python str_sims.py -m $mu -b ${beta[9]} -p $path -w 10
