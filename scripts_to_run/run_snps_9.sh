4cb5c4304eb1635e760779cdbae8216f927a80ce
ecgberg <eglassbe@stanford.edu>
2015-08-26 11:58:02 -0700

    new scripts - edited for nearly neutral or high mutation cases

--- scripts_to_run/run_snps_9.sh
+++ scripts_to_run/run_snps_9.sh
@@ -21,21 +21,21 @@
 
 module load python/2.7
 
-mu=.0005
-
+mu=.05
+sf=1
 beta=( 1 0.9 0.8 0.7 0.6 0.5 0.4 0.2 0.3 0.1 )
 
 runnum=9
 
 path='/home/eglassbe/str_simulations/str_res/run_'$runnum'/'
 
-python str_sims.py -m $mu -b ${beta[0]} -p $path -w 10
-python str_sims.py -m $mu -b ${beta[1]} -p $path -w 10
-python str_sims.py -m $mu -b ${beta[2]} -p $path -w 10
-python str_sims.py -m $mu -b ${beta[3]} -p $path -w 10
-python str_sims.py -m $mu -b ${beta[4]} -p $path -w 10
-python str_sims.py -m $mu -b ${beta[5]} -p $path -w 10
-python str_sims.py -m $mu -b ${beta[6]} -p $path -w 10
-python str_sims.py -m $mu -b ${beta[7]} -p $path -w 10
-python str_sims.py -m $mu -b ${beta[8]} -p $path -w 10
-python str_sims.py -m $mu -b ${beta[9]} -p $path -w 10
+python str_sims.py -m $mu -b ${beta[0]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu -b ${beta[1]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu -b ${beta[2]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu -b ${beta[3]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu -b ${beta[4]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu -b ${beta[5]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu -b ${beta[6]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu -b ${beta[7]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu -b ${beta[8]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu -b ${beta[9]} -p $path -w 100 -f $sf
--- scripts_to_run/run_strs_3.sh
+++ scripts_to_run/run_strs_3.sh
@@ -21,29 +21,23 @@
 
 module load python/2.7
 
-mu=.0005
+mu=.05
 p=0.84
 sigsq_g=20
-
-beta=0.5
-
-path='/home/eglassbe/str_simulations/str_res/'
-
-python str_sims.py -m $mu $p $sigsq_g -b $beta -p $path
-
+sf=1
 beta=( 0.5 0.45 0.4 0.35 0.3 0.25 0.2 0.1 0.15 0.05 )
 
 runnum=3
 
 path='/home/eglassbe/str_simulations/str_res/run_'$runnum'/'
 
-python str_sims.py -m $mu $p $sigsq_g -b ${beta[0]} -p $path -w 10
-python str_sims.py -m $mu $p $sigsq_g -b ${beta[1]} -p $path -w 10
-python str_sims.py -m $mu $p $sigsq_g -b ${beta[2]} -p $path -w 10
-python str_sims.py -m $mu $p $sigsq_g -b ${beta[3]} -p $path -w 10
-python str_sims.py -m $mu $p $sigsq_g -b ${beta[4]} -p $path -w 10
-python str_sims.py -m $mu $p $sigsq_g -b ${beta[5]} -p $path -w 10
-python str_sims.py -m $mu $p $sigsq_g -b ${beta[6]} -p $path -w 10
-python str_sims.py -m $mu $p $sigsq_g -b ${beta[7]} -p $path -w 10
-python str_sims.py -m $mu $p $sigsq_g -b ${beta[8]} -p $path -w 10
-python str_sims.py -m $mu $p $sigsq_g -b ${beta[9]} -p $path -w 10
+python str_sims.py -m $mu $p $sigsq_g -b ${beta[0]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu $p $sigsq_g -b ${beta[1]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu $p $sigsq_g -b ${beta[2]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu $p $sigsq_g -b ${beta[3]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu $p $sigsq_g -b ${beta[4]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu $p $sigsq_g -b ${beta[5]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu $p $sigsq_g -b ${beta[6]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu $p $sigsq_g -b ${beta[7]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu $p $sigsq_g -b ${beta[8]} -p $path -w 100 -f $sf
+python str_sims.py -m $mu $p $sigsq_g -b ${beta[9]} -p $path -w 100 -f $sf
